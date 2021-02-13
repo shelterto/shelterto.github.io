65465b23256d09b91d35fae69355623c6cbee690
Arlene Wang <wang.s.arlene@gmail.com>
2021-02-13 18:50:30 -0500

    location header initial

--- src/infoCard/InfoCard.js
+++ src/infoCard/InfoCard.js
@@ -1,24 +1,33 @@
-import '../infoCard/InfoCard.css'
+import "../infoCard/InfoCard.css";
 
-const InfoCard = ({shelterName, shelterType, shelterOccupancy, shelterCapacity}) => {
-    
-
-
-    return(
-        <div className="infoCard"> 
-             <div className="row justify-content-md-center">
-                <div className="col-md-3 textGrid">
-                    <h1>{shelterName}</h1>
-                    <h1 className="shelterType"><b>{shelterType}</b></h1>
-                </div>
-                <div className="col-md-3 occupancyGrid">
-                    <h3>Occupancy: {shelterOccupancy}</h3>
-                    <h3>Capacity: </h3>
-                    <button class="submitBtn" >Location</button>
-                </div>
-            </div>
+const InfoCard = ({
+  shelterName,
+  shelterType,
+  shelterOccupancy,
+  shelterCapacity,
+  shelterLink,
+}) => {
+  return (
+    <div className="infoCard">
+      <div className="row justify-content-md-center">
+        <div className="col-md-3 textGrid">
+          <h1>{shelterName}</h1>
+          <h1 className="shelterType">
+            <b>{shelterType}</b>
+          </h1>
         </div>
-    )
-}
+        <div className="col-md-3 occupancyGrid">
+          <h3>Occupancy: {shelterOccupancy}</h3>
+          <h3>Capacity: </h3>
+          <button class="submitBtn">Location</button>
+
+          {/* <a href="{shelterLink}">
+                        <button>Open in Google Maps</button>
+                    </a> */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default InfoCard;
--- src/locationHeader/LocationHeader.js
+++ src/locationHeader/LocationHeader.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import '../locationHeader/LocationHeader.css'
+import React from "react";
+import "../locationHeader/LocationHeader.css";
 
-const LocationHeader = () => {
-    return(
-        <div>
-
-        </div>
-    )
-
-}
+const LocationHeader = ({ shelterAddress }) => {
+  return (
+    <div className="locationHeader">
+      <h1>{shelterAddress}</h1>
+    </div>
+  );
+};
 
 export default LocationHeader;
