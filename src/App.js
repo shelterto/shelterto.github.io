49045757462202a0a08fd6c700ec26eeef2d3111
BrianErlangga <brianerlangga15@gmail.com>
2021-02-13 16:01:29 -0500

    added boostraps pls npm install

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import utensilsImg from './images/utensils.svg'
 import './App.css';
 import InfoCard from './infoCard/InfoCard'
 import DonoCard from './donoCard/DonoCard'
+import ParentDonoContainer from './parentDonoContainer/parentDonoContainer'
 
 function App() {
 
   return (
     <div className="App">
       <DonoCard DonoColor="green" DonoText="Food" DonoImg={utensilsImg}/>
+      <ParentDonoContainer />
     </div>
   );
 }
---
+++ src/parentDonoContainer/parentDonoContainer.js
@@ -0,0 +1,14 @@
+import '../parentDonoContainer/parentDonoContainer.css'
+
+const parentDonoContainer = () => {
+    
+
+    return(
+        <div> 
+            <h1> Hello World  </h1>
+            <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+    )
+}
+
+export default parentDonoContainer;
