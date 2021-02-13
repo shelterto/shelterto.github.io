980f59c95a9185c610651960a0a72ea66e2d9a7f
BrianErlangga <brianerlangga15@gmail.com>
2021-02-13 18:49:19 -0500

    added scroll

--- src/infoCardContainer/infoCardContainer.js
+++ src/infoCardContainer/infoCardContainer.js
@@ -4,7 +4,7 @@ import InfoCard from '../infoCard/InfoCard'
 
 const infoCardContainer = () => {
     return(
-        <div className="infoCardContainer">
+        <div className="infoCardContainer" id="like-scroll">
             <InfoCard shelterName="Toronto Community Center" shelterType="Women" shelterOccupancy="1050" shelterCapacity="2000"/>
             <InfoCard shelterName="Toronto Community Center" shelterType="Women"/>
             <InfoCard shelterName="Toronto Community Center" shelterType="Women"/>
