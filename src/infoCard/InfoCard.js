5c8c4452f4f19946269ac8118002b2740d0aafbd
BrianErlangga <brianerlangga15@gmail.com>
2021-02-14 00:29:24 -0500

    search style .seachBox class to style

--- src/infoCard/InfoCard.js
+++ src/infoCard/InfoCard.js
@@ -9,7 +9,22 @@ const InfoCard = ({
   onClick,
 }) => {
 
-
+    var str1 = "gray";
+    var str2 = "gray";
+    var str3 = "gray";
+    if(shelterOccupancy == 1) {
+      str1 = "#A9E062";
+      str2 = "#D7D7D7";
+      str3 = "#D7D7D7";
+    } else if(shelterOccupancy == 2) {
+      str1 = "#F9BE26";
+      str2 = "#F9BE26";
+      str3 = "#D7D7D7";
+    } else {
+      str1 = "#F63D3D";
+      str2 = "#F63D3D";
+      str3 = "#F63D3D";
+    }
   return (
     <div className="infoCard">
       <div className="row justify-content-md-center">
@@ -21,9 +36,9 @@ const InfoCard = ({
         </div>
         <div className="col-md-5 occupancyGrid">
           <h3>Capacity: {shelterCapacity}</h3>
-          <h3>Occupancy: <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 1) ? 'green' : 'gray')} width = "34" height = "34" viewBox="0 0 640 512"><path fill={(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 1) ? 'green' : 'gray')} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 2) ? 'yellow' : 'gray')} width = "34" height = "34" viewBox="0 0 640 512"><path fill={(shelterOccupancy == 3) ? 'red' : (shelterOccupancy == 1) ? 'green' : 'gray'} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 3) ? 'red' : 'gray')} width = "34" height = "34" viewBox="0 0 640 512"><path fill={(shelterOccupancy == 3) ? 'red' : (shelterOccupancy == 1) ? 'green' : 'gray'} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
+          <h3>Occupancy: <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {str1} width = "34" height = "34" viewBox="0 0 640 512"><path fill={str1} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {str2} width = "34" height = "34" viewBox="0 0 640 512"><path fill={str2} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {str3} width = "34" height = "34" viewBox="0 0 640 512"><path fill={str3} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
            </h3>
            <button class="submitBtn" onClick={onClick}>Location</button> 
         </div>
--- src/infoCardContainer/infoCardContainer.js
+++ src/infoCardContainer/infoCardContainer.js
@@ -4,17 +4,37 @@ import LocationHeader from "../locationHeader/LocationHeader"
 import algoliasearch from 'algoliasearch/lite';
 import {
   InstantSearch,
-  Hits,
-  SearchBox,
+  InfiniteHits,
   Pagination,
   Highlight,
   ClearRefinements,
   RefinementList,
   Configure,
+  connectSearchBox
 } from 'react-instantsearch-dom';
 import PropTypes from 'prop-types';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const searchClient = algoliasearch('A8LQ861XZY', '0b476f98a04f33f57d6f0b459a403111');
+const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
+    <form noValidate action="" role="search" >
+      <input
+        class="searchBox"
+        type="search"
+        value={currentRefinement}
+        onChange={event => refine(event.currentTarget.value)}
+      />
+      <button onClick={() => refine('')}>Reset query</button>
+      {isSearchStalled ? 'My search is stalled' : ''}
+    </form>
+  );
+
+const CustomSearchBox = connectSearchBox(SearchBox);
+
+const mapStyles = {
+    width: "643px",
+    height: '100%',
+  };
 
 const InfoCardContainer = () => {
     const [items, setItems] = useState([]);
@@ -55,24 +75,30 @@ const InfoCardContainer = () => {
                 <div className="ais-InstantSearch">
                 <InstantSearch indexName="demo_geo_3" searchClient={searchClient}>
                 <div className="right-panel">
-                    <SearchBox />
+                    <CustomSearchBox />
                     <div className="infoCardContainer"  id="like-scroll">
-                        <Hits hitComponent={Hit} />
+                        <InfiniteHits hitComponent={Hit} />
                     </div>
                 </div>
                 </InstantSearch>
                 </div>
             </div>
             <div col-md-6>
+                    {/* <Map
+                    google={this.propsgoogle}
+                    zoom={8}
+                    style={mapStyles}
+                    initialCenter={{ lat: 47.444, lng: -122.176}}
+                    /> */}
                     <LocationHeader shelterAddress={address} />
             </div>
         </div>
     </div>
-
-       
     )
 }
 
+// export default InfoCardContainer;
 
-
-export default InfoCardContainer;
+export default GoogleApiWrapper({
+    apiKey: 'TOKEN HERE'
+  })(InfoCardContainer);
