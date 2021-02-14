59d85666048fca909244404bfca8df593db8619b
BrianErlangga <brianerlangga15@gmail.com>
2021-02-13 23:07:05 -0500

    stuff changed

--- src/infoCard/InfoCard.js
+++ src/infoCard/InfoCard.js
@@ -6,7 +6,7 @@ const InfoCard = ({
   shelterType,
   shelterOccupancy,
   shelterCapacity,
-  shelterLink,
+  onClick,
 }) => {
 
 
@@ -21,15 +21,11 @@ const InfoCard = ({
         </div>
         <div className="col-md-5 occupancyGrid">
           <h3>Capacity: {shelterCapacity}</h3>
-          <h3>Occupancy: <img src = {bed} class = {(shelterOccupancy == 3) ? 'occupancyred' : (shelterOccupancy == 3) ? 'occupancygreen' : 'occupancygray'}/>
-          <img src = {bed} class = {(shelterOccupancy == 3) ? 'occupancyred' : (shelterOccupancy == 2) ? 'occupancygreen' : 'occupancygray'}/>
-          <img src = {bed} class = {(shelterOccupancy == 3) ? 'occupancyred' : (shelterOccupancy == 3) ? 'occupancygreen' : 'occupancygray'}/>
+          <h3>Occupancy: <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 1) ? 'green' : 'gray')} width = "34" height = "34" viewBox="0 0 640 512"><path fill={(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 1) ? 'green' : 'gray')} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 2) ? 'yellow' : 'gray')} width = "34" height = "34" viewBox="0 0 640 512"><path fill={(shelterOccupancy == 3) ? 'red' : (shelterOccupancy == 1) ? 'green' : 'gray'} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bed" class="svg-inline--fa fa-bed fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" color = {(shelterOccupancy == 3) ? 'red' : ((shelterOccupancy == 3) ? 'red' : 'gray')} width = "34" height = "34" viewBox="0 0 640 512"><path fill={(shelterOccupancy == 3) ? 'red' : (shelterOccupancy == 1) ? 'green' : 'gray'} d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"></path></svg>
            </h3>
-          <button class="submitBtn">Location</button>
-
-          {/* <a href="{shelterLink}">
-                        <button>Open in Google Maps</button>
-                    </a> */}
+           <button class="submitBtn" onClick={onClick}>Location</button> 
         </div>
       </div>
     </div>
--- src/infoCardContainer/infoCardContainer.js
+++ src/infoCardContainer/infoCardContainer.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import '../infoCardContainer/infoCardContainer.css'
+import React, {useState, useEffect} from 'react';import '../infoCardContainer/infoCardContainer.css'
 import InfoCard from '../infoCard/InfoCard'
+import LocationHeader from "../locationHeader/LocationHeader"
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 
-const infoCardContainer = () => {
+const searchClient = algoliasearch('A8LQ861XZY', '0b476f98a04f33f57d6f0b459a403111');
+
+const InfoCardContainer = () => {
+    const [items, setItems] = useState([]);
+    const [address, setAddress] = useState("Please select a location");
+
+    useEffect (() => {
+        const fetchData = async () => {
+          const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+          const data = await response.json();
+          setItems(data)   
+        };
+        fetchData();
+        console.log(items)
+      },[])
+
+      const changeLocationHeader = (address) => {
+        setAddress(address);
+        console.log(address)
+    }
+
+    function Hit(props) {
+        return (  
+          <div className="infoCardContainer"  id="like-scroll">
+            <InfoCard key={props.hit.objectID} shelterName={props.hit.FACILITY_NAME} shelterType={props.hit.SECTOR} shelterOccupancy={props.hit.OCCUPANCY} shelterCapacity={props.hit.CAPACITY} onClick={() => changeLocationHeader(props.hit.SHELTER_ADDRESS
+        )}/>
+          </div>
+        );
+      }
+      
+      Hit.propTypes = {
+        hit: PropTypes.object.isRequired,
+      };
+
     return(
-        <div className="infoCardContainer" id="like-scroll">
-            <InfoCard shelterName="Kevins sintky home arlene hnds " shelterType="Women" shelterOccupancy="10" shelterCapacity="2000"/>
-            <InfoCard shelterName="Toronto Community Center" shelterType="Women" shelterOccupancy="10" shelterCapacity="2000"/>
-            <InfoCard shelterName="Toronto Community Center" shelterType="Women" shelterOccupancy="10" shelterCapacity="2000"/>
-            <InfoCard shelterName="Toronto Community Center" shelterType="Women" shelterOccupancy="10" shelterCapacity="2000"/>
+    <div>
+        <div className="row">
+            <div col-md-6>
+                <div className="ais-InstantSearch">
+                <InstantSearch indexName="demo_geo_3" searchClient={searchClient}>
+                <div className="right-panel">
+                    <SearchBox />
+                    <div className="infoCardContainer"  id="like-scroll">
+                        <Hits hitComponent={Hit} />
+                    </div>
+                </div>
+                </InstantSearch>
+                </div>
+            </div>
+            <div col-md-6>
+                    <LocationHeader shelterAddress={address} />
+            </div>
         </div>
-    )
+    </div>
 
+       
+    )
 }
 
-export default infoCardContainer;
+
+
+export default InfoCardContainer;
